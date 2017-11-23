4f5f52830ec1938764e302b3555205662898af61
agingrasc <alexandre.gingras-courchesne.1@ulaval.ca>
2017-11-23 18:16:31 -0500

    add startup_stm32.s

--- src/main.c
+++ src/main.c
@@ -1,20 +1,25 @@
+#include <stm32f4xx_conf.h>
 #include "stm32f4xx_rcc.h"
 #include "stm32f4xx_gpio.h"
 #include "stm32f4xx_conf.h"
 
 
 int main() {
+    SystemInit();
+
     GPIO_InitTypeDef gpio_init;
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
     gpio_init.GPIO_Mode = GPIO_Mode_OUT;
     gpio_init.GPIO_Pin = GPIO_Pin_15;
+    gpio_init.GPIO_Speed = GPIO_Speed_100MHz;
+    gpio_init.GPIO_OType = GPIO_OType_PP;
+    gpio_init.GPIO_PuPd = GPIO_PuPd_NOPULL;
     GPIO_Init(GPIOD, &gpio_init);
 
+    GPIO_ResetBits(GPIOD, GPIO_Pin_15);
+
     while(1) {
-        for (int i = 0; i < 10000; i++) {
-            0;
-        }
-        GPIO_ToggleBits(GPIOD, GPIO_Pin_15);
+        GPIO_SetBits(GPIOD, GPIO_Pin_15);
     }
 }
--- toolchain-arm-none-eabi.cmake
+++ toolchain-arm-none-eabi.cmake
@@ -5,6 +5,7 @@ SET(CMAKE_CXX_COMPILER /opt/arm-none-eabi/bin/arm-none-eabi-g++)
 SET(CMAKE_ASM_COMPILER /opt/arm-none-eabi/bin/arm-none-eabi-as)
 SET(CMAKE_OBJCOPY "/opt/arm-none-eabi/bin/arm-none-eabi-objcopy" CACHE FILEPATH "Cross toolchain objcopy command" FORCE)
 
-SET(CMAKE_EXE_LINKER_FLAGS "--specs=nosys.specs" CACHE INTERNAL "")
+set(LINKER_SCRIPT "/home/jenifael/git/fahrerlos-embedded/stm32_flash.ld" CACHE STRING "")
+SET(CMAKE_EXE_LINKER_FLAGS "--specs=nosys.specs -T ${LINKER_SCRIPT} -nostartfiles" CACHE INTERNAL "")
 
 SET(CMAKE_FIND_ROOT_PATH /opt/arm-none-eabi/arm-none-eabi)
